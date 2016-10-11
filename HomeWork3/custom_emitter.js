c8b3edff5350386802e10a752a23d5f34d130a0a
Maysam Gamini <Maysam Gamini>
2016-10-10 22:31:52 -0600

    check Emitter

--- HomeWork3/custom_emitter.js
+++ HomeWork3/custom_emitter.js
@@ -1,2 +1,12 @@
-var Emitter = require('.emitter');
+var Emitter = require('./emitter');
 var emit = new Emitter();
+
+emit.on('greet',function(){
+    console.log('Hi From CS572');
+});
+emit.on('greet',function(){
+    console.log('Hi from CS472');
+});
+
+console.log('Hello!');
+emit.emit('greet');
